a5e6f1aece7abbfed2ad2b921ec607f54c05cb0c
hohenstaufen <hydroxide.lithium@gmail.com>
2024-07-23 10:45:40 +0400

    spacing works

--- drawing_plots.py
+++ drawing_plots.py
@@ -4,10 +4,7 @@ import matplotlib.pyplot as plt
 import json
 #class plot_drawer
 
-with open('.\data\data.json', 'r') as f:
-    data = json.load(f)
-df = pd.DataFrame(data)
-
+df = pd.read_json("./data/deviations.json")
 
 
 columns = df['gt_corners'].unique()
@@ -31,11 +28,47 @@ rows = [
 
 
 
-for iteration in columns: 
-    filtered_df = df[df["gt_corners"].isin([iteration])].filter(items=rows)
-    filtered_df.hist()
-    # plt.figure(figsize=(10,5))
-    plt.show()
+# for iteration in columns: 
+#     filtered_df = df[df["gt_corners"].isin([iteration])].filter(items=rows)
+#     plt.figure(figsize=(10,7))
+#     filtered_df.hist()
+#     plt.show()  
+
+
+
+
+filtered_df = df[df["gt_corners"].isin([columns[0]])].filter(items=rows)
+#plt.figure(figsize=(10,7))
+#plt.hist(filtered_df[rows[2]])
+
+
+#plt.figure(figsize=(10,7))
+
+fig, axs = plt.subplots(3, 3, layout="constrained")
+fig.suptitle('deviation histograms for corners = ' + str(columns[0]) + '\n',
+          fontweight = "bold")
+fig.set_size_inches(18.5, 10.5)
+
+# axs.set_xlabel('Deviation value')
+# axs.set_ylabel('Frequency')
+
+fig.text(0.5, 0.04, 'Deviation value', ha='center', va='center')
+fig.text(0.06, 0.5, 'Frequency', ha='center', va='center', rotation='vertical')
+
+
+for i in range(3):
+    for j in range(3):
+       axs[i, j].hist(filtered_df[rows[3*i+j]])
+       axs[i, j].set_title('\n\n'+ str(rows[3*i+j]), fontfamily = "serif" , loc = "left" , fontsize = "medium")
+       
+plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1)
+
+# axs[0, 0].hist(filtered_df[rows[2]])
+# axs[0, 0].set_title('A single plot')
+# axs[0, 1].hist(filtered_df[rows[3]])
+# axs[0, 1].set_title('A single plot')
+plt.show()
+
 
 # i=0
 # j=0
@@ -47,3 +80,18 @@ for iteration in columns:
 
 
 
+
+
+class drawing_plots:
+
+
+    def draw_plots():
+        
+        #draw comparison plots:
+        ## histograms for corner number
+        ## histograms for gt/rb corners
+        ## 
+
+        #saves as files
+        #return paths
+        return 0
